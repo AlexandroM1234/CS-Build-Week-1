6cbc8df1b6b49761a041da451eb02933c3255577
Alex Martinez <alexmartinez0071234@gmail.com>
2020-07-28 22:34:54 -0400

    logic fixed so the hanging stopped and clear/rndm

--- game-of-life/src/App.js
+++ game-of-life/src/App.js
@@ -1,37 +1,110 @@
-import React, { useState } from "react";
-import randomColor from "random-color";
+import React, { useState, useCallback, useRef } from "react";
 import produce from "immer";
-import "./App.css";
-
-const numRow = 50;
+import randomColor from "random-color";
+const numRows = 50;
 const numCols = 50;
 let colors = randomColor();
 
+const operations = [
+  [0, 1],
+  [0, -1],
+  [1, -1],
+  [-1, 1],
+  [1, 1],
+  [-1, -1],
+  [1, 0],
+  [-1, 0],
+];
+
+const generateEmptyGrid = () => {
+  const rows = [];
+  for (let i = 0; i < numRows; i++) {
+    rows.push(Array.from(Array(numCols), () => 0));
+  }
+
+  return rows;
+};
+
 function App() {
   const [grid, setGrid] = useState(() => {
-    const rows = [];
-    for (let i = 0; i < numRow; i++) {
-      rows.push(Array.from(Array(numCols), () => 0));
-    }
-    return rows;
+    return generateEmptyGrid();
   });
 
   const [running, setRunning] = useState(false);
+
+  const runningRef = useRef(running);
+  runningRef.current = running;
+
+  const runSimulation = useCallback(() => {
+    if (!runningRef.current) {
+      return;
+    }
+
+    setGrid((g) => {
+      return produce(g, (gridCopy) => {
+        for (let i = 0; i < numRows; i++) {
+          for (let k = 0; k < numCols; k++) {
+            let neighbors = 0;
+            operations.forEach(([x, y]) => {
+              const newI = i + x;
+              const newK = k + y;
+              if (newI >= 0 && newI < numRows && newK >= 0 && newK < numCols) {
+                neighbors += g[newI][newK];
+              }
+            });
+
+            if (neighbors < 2 || neighbors > 3) {
+              gridCopy[i][k] = 0;
+            } else if (g[i][k] === 0 && neighbors === 3) {
+              gridCopy[i][k] = 1;
+            }
+          }
+        }
+      });
+    });
+
+    setTimeout(runSimulation, 100);
+  }, []);
+
   return (
-    <div>
+    <>
       <button
         onClick={() => {
           setRunning(!running);
+          if (!running) {
+            runningRef.current = true;
+            runSimulation();
+          }
         }}
       >
         {running ? "stop" : "start"}
       </button>
+      <button
+        onClick={() => {
+          const rows = [];
+          for (let i = 0; i < numRows; i++) {
+            rows.push(
+              Array.from(Array(numCols), () => (Math.random() > 0.7 ? 1 : 0))
+            );
+          }
+
+          setGrid(rows);
+        }}
+      >
+        random
+      </button>
+      <button
+        onClick={() => {
+          setGrid(generateEmptyGrid());
+        }}
+      >
+        clear
+      </button>
       <div
         style={{
           display: "grid",
-          gridTemplateColumns: `repeat(${numCols},25px)`,
+          gridTemplateColumns: `repeat(${numCols}, 20px)`,
         }}
-        className="App"
       >
         {grid.map((rows, i) =>
           rows.map((col, k) => (
@@ -44,10 +117,10 @@ function App() {
                 setGrid(newGrid);
               }}
               style={{
-                width: 25,
-                height: 25,
+                width: 20,
+                height: 20,
                 backgroundColor: grid[i][k]
-                  ? `${colors.hexString()}`
+                  ? `${colors.rgbString()}`
                   : undefined,
                 border: "solid 1px black",
               }}
@@ -55,7 +128,7 @@ function App() {
           ))
         )}
       </div>
-    </div>
+    </>
   );
 }
 
